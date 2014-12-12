a7bc7b3c46222a98abacb458cc6c615cb04ad15e
momosundeass <s7070055@kmitl.ac.th>
2014-12-13 03:30:17 +0700

    v3.4

--- project v1.py
+++ project v1.py
@@ -1,5 +1,7 @@
 from Tkinter import *
+from random import *
 master = Tk()
+master.title('Shortest Path')
 ######################### Class Area
 class Position:
     def __init__(self, posx, posy):
@@ -59,7 +61,7 @@ def convert_list_dict(lis):
         else:
             dic[c[0]] += [c[1]]
     return dic
-            
+
 ######################### Third Frame
 def changeposmodule(event):
     if len(w_dot) != 0:
@@ -69,34 +71,29 @@ def changeposmodule(event):
         line_destroy()
 def calcu():
     global graph_dic
-    print graph_dic
-    print p1.get()
-    print p2.get()
     temp = find_shortest_path(graph_dic, p1.get(), p2.get(), path=[])
-    Label(master, text=str(temp)).grid(column=3,row=3)
-Label(master, text='From(Number)').grid(row=0, column=3, sticky=W)
+Label(master, text='Find Shortest Path From').grid(row=0, column=3, sticky=W)
 p1 = Entry(master, width=2)
-p1.grid(row=0, column=3, sticky=W, padx=100)
-Label(master, text='To(Number)').grid(row=0, column=3, sticky=W, padx=120)
+p1.grid(row=0, column=3, sticky=W, padx=135)
+Label(master, text='To').grid(row=0, column=3, sticky=W, padx=155)
 p2 = Entry(master, width=2)
-p2.grid(row=0, column=3, sticky=W, padx=200)
+p2.grid(row=0, column=3, sticky=W, padx=175)
 Button(master, text='Calculate', command=calcu).grid(row=0, column=3, sticky=E)
-w = Canvas(master, width=600, height=600, bg='PINK')
+w = Canvas(master, width=500, height=500, bg='PINK')
 w.grid(row=2, rowspan=1000, column=3)
 w.bind("<Button-1>", changeposmodule)
 
 ######################### First Frame
 def add_button():
     global i
-##    a.append(Label(ftop, text='Node'+'%02d' % int(1+i)))
     a.append(Label(ftop, text=z(i+1)))
     b.append(Entry(ftop))
-    w_pos.append(Position(300, 300))
+    pos = Position(randrange(50, 450), randrange(50, 450))
+    w_pos.append(Position(pos.x, pos.y))
     w_dot.append(w.create_rectangle(w_pos[i].x-2, w_pos[i].y-2, w_pos[i].x+2, w_pos[i].y+2, fill='BLACK'))
-    w_name.append(w.create_text(300, 290, text=z(i+1) ))
-##    w_name.append(w.create_text(300, 290, text=str(i+1)))
-    a[i].grid(row=i+1, column=0, sticky=W)
-    b[i].grid(row=i+1, padx=30, sticky=W)
+    w_name.append(w.create_text(pos.x, pos.y - 10, text=z(i+1) ))
+    a[i].grid(row=i+2, column=0, sticky=W)
+    b[i].grid(row=i+2, padx=30, sticky=W)
     line_destroy()
     i += 1
 def del_button():
@@ -114,6 +111,7 @@ def del_button():
         line_destroy()
         i -= 1
 ftop = Frame(master).grid(row=0)
+Label(ftop, text='Comment').grid(row=1)
 Button(ftop, text="Add Node", command=add_button).grid(row=0, sticky=W)
 Button(ftop, text="Delete Node", command=del_button).grid(row=0, sticky=W, padx=70)
 
@@ -121,11 +119,14 @@ Button(ftop, text="Delete Node", command=del_button).grid(row=0, sticky=W, padx=
 ######################### Second Frame
 def add_line():
     global i2
+    global d
     a2.append(Entry(f2top,width=5))
     b2.append(Entry(f2top,width=5))
     a2[i2].grid(row=i2+2, column=2, sticky=W)
     b2[i2].grid(row=i2+2, column=2, sticky=W, padx=50)
     line_destroy()
+    d.grid_remove()
+    d.grid(row=i2+3, column=2, sticky=E)
     i2 += 1
 def del_line():
     global i2
@@ -135,6 +136,10 @@ def del_line():
         a2.pop()
         b2[i2-1].destroy()
         b2.pop()
+        d.grid_remove()
+        d.grid(row=i2+3, column=2, sticky=E)
+        if len(a2) == 0:
+            d.grid_remove()
         i2 -= 1
 def line_c():
     global graph
@@ -152,6 +157,6 @@ Button(f2top, text="add_line", command=add_line).grid(row=0, column=2, sticky=W)
 Button(f2top, text="del_line", command=del_line).grid(row=0, column=2, sticky=W, padx=60)
 Label(f2top, text='Node To').grid(row=1, column=2, sticky=W)
 Label(f2top, text='Node').grid(row=1, column=2, sticky=W, padx=50)
-Button(f2top, text='create line', command=line_c).grid(row=0, column=2, sticky=W, padx=115)
+d = Button(f2top, text='create line', command=line_c)
 
 master.mainloop()
